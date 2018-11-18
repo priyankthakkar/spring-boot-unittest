addfa3e056115da1dbf4923d85f9114aead39f40
Priyank Thakkar <priyankthakkar@gmail.com>
2018-11-18 19:04:48 +1100

    Change Log: 1. Basic unit test case for Hello World controller Changes By: Priyank Thakkar

---
+++ src/main/java/io/priyank/springbootut/SpringbootUtApplication.java
@@ -0,0 +1,12 @@
+package io.priyank.springbootut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootUtApplication.class, args);
+    }
+}
